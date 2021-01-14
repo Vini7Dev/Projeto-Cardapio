ef3075a9bece8001eb61b2b22e38c5f4ec6200f2
Vinícius Gabriel <68403363+Vinicius7Dev@users.noreply.github.com>
2021-01-14 11:44:34 -0300

    Criando a página "CreateFood" - Parte 1

--- web/src/pages/CreateFood/index.tsx
+++ web/src/pages/CreateFood/index.tsx
@@ -3,14 +3,94 @@
  */
 
 import React from 'react';
+import { FiLogOut, FiArrowLeft, FiCamera, FiX } from 'react-icons/fi';
+
+import MenuHeader from '../../components/PageElements/MenuHeader';
+import MenuFooter from '../../components/PageElements/MenuFooter';
+import OptionsBar from '../../components/PageElements/OptionsBar';
+
+import Button from '../../components/InputAndButtons/Button';
 
 // Component styles
-import { Container } from './styles';
+import {
+    Container,
+    CreateItemSide,
+    CreateItemArea,
+} from './styles';
 
 const CreateFood: React.FC = () => {
     return (
       <Container>
-        <h1>CreateFood</h1>
+        <OptionsBar
+          buttonsArray={[
+            { text: 'Sair', icon: FiLogOut, action: () => console.log('OK') },
+            { text: 'Voltar', icon: FiArrowLeft, action: () => console.log('OK') },
+          ]}
+        />
+
+        <CreateItemSide>
+          <MenuHeader
+            title="Hello"
+          />
+
+          <CreateItemArea>
+            <div id="add-item-image">
+              <div id="image-preview" />
+
+              <button>
+                <FiCamera size={25} />
+
+                Adicionar imagem
+              </button>
+            </div>
+
+            <div className="add-item-input">
+              <strong>Informe o nome do prato:</strong>
+              <input type="text" />
+            </div>
+
+            <div id="add-item-textarea">
+              <strong>Informe a descrição do prato:</strong>
+              <textarea cols={30} rows={3} style={{ resize: 'none' }} />
+            </div>
+
+            <div className="add-item-input">
+              <strong>Informe o preço original do prato:</strong>
+              <input type="text" />
+            </div>
+
+            <div className="add-item-input">
+              <strong>Informe o preço com desconto (se houver):</strong>
+              <input type="text" />
+            </div>
+
+            <div className="add-item-input">
+              <strong>Informe a categoria do prato:</strong>
+              <input type="text" />
+            </div>
+
+            <div id="add-enable-checkbox">
+              <strong>Habilitado</strong>
+
+              <span id="custom-checkbox">
+                <FiX />
+              </span>
+            </div>
+          </CreateItemArea>
+
+          <div id="add-item-button">
+            <Button
+              buttonName="Adicionar item"
+              size="normal"
+            />
+          </div>
+
+          <MenuFooter
+            trade="Fome Burguer"
+            cnpj="12345678910234"
+            telephone="16999999999"
+          />
+        </CreateItemSide>
       </Container>
     );
 }
--- web/src/pages/CreateFood/styles.ts
+++ web/src/pages/CreateFood/styles.ts
@@ -4,4 +4,190 @@
 
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+import DefaultFoodImage from '../../assets/images/DefaultFoodImage.png';
+
+export const Container = styled.div`
+    width: 98.91%;
+    height: 100%;
+
+    display: flex;
+    justify-content: start;
+
+    @media (max-width: 768px) {
+        display: block;
+        width: 100%;
+    }
+`;
+
+export const CreateItemSide = styled.main`
+    background-color: #E6E6E6;
+    width: 100%;
+    height: 100%;
+
+    div#add-item-button {
+        width: 95%;
+        margin: 0 auto;
+    }
+`;
+
+export const CreateItemArea = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    width: 95%;
+    margin: 25px auto;
+    border-radius: 10px;
+
+    background-color: #FFFFFF;
+    box-shadow: 1px 1px 3px #757575;
+
+    div#add-item-image {
+        display: flex;
+        flex-direction: column;
+
+        width: 100%;
+        height: 100%;
+        max-height: 250px;
+
+        div#image-preview {
+            height: 250px;
+
+            background: url(${DefaultFoodImage}) no-repeat center;
+        }
+
+        margin-bottom: 50px;
+        border-radius: 10px 10px 0 0;
+
+        background-color: #FF7A00;
+
+        button {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+            width: 35%;
+            max-width: 300px;
+            height: 50px;
+
+            padding: 0 10px;
+
+            border: none;
+            border-radius: 25px;
+            margin: 0 auto -25px;
+
+            box-shadow: 1px 1px 3px #000000;
+            background-color: #39B100;
+            color: #FFFFFF;
+
+            font-family: 'Poppins', 'sans-serif';
+            font-size: 20px;
+            line-height: 15px;
+
+            svg {
+                margin-right: 5px;
+            }
+        }
+    }
+
+    div.add-item-input {
+        width: 90%;
+
+        margin-bottom: 25px;
+
+        strong {
+            margin-left: 15px;
+
+            font-size: 25px;
+        }
+
+        input {
+            width: 100%;
+            min-height: 50px;
+
+            padding: 0 15px;
+
+            border: none;
+            border-radius: 25px;
+
+            box-shadow: 1px 1px 3px #757575;
+            background-color: #ECECEC;
+
+            font-family: 'Poppins', 'sans-serif';
+            font-size: 20px;
+        }
+    }
+
+    div#add-item-textarea {
+        width: 90%;
+
+        margin-bottom: 25px;
+
+        strong {
+            margin-left: 15px;
+
+            font-size: 25px;
+        }
+
+        textarea {
+            width: 100%;
+            min-height: 50px;
+
+            padding: 15px;
+
+            border: none;
+            border-radius: 25px;
+
+            box-shadow: 1px 1px 3px #757575;
+            background-color: #ECECEC;
+
+            font-family: 'Poppins', 'sans-serif';
+            font-size: 20px;
+        }
+    }
+
+    div#add-enable-checkbox {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        strong {
+            margin-right: 10px;
+            font-size: 25px;
+        }
+
+        span#custom-checkbox {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+            width: 40px;
+            height: 40px;
+
+            border-radius: 5px;
+
+            background-color: #39B100;
+            color: #FFFFFF;
+
+            font-size: 50px;
+            font-family: 'Archivo', 'sans-serif';
+        }
+
+        margin-bottom: 25px;
+    }
+
+    @media (max-width: 768px) {
+        div#add-item-image {
+            div#image-preview {
+                height: 150px;
+                background-size: 75%;
+            }
+
+            button {
+                width: 80%;
+                font-size: 15px;
+            }
+        }
+    }
+`;
