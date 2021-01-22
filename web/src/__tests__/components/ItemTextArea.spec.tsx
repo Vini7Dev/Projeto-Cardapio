8d1bf5951c198dc0bbad46ce97a2010367387483
Vinícius Gabriel <68403363+Vinicius7Dev@users.noreply.github.com>
2021-01-22 10:32:07 -0300

    Criando o teste do componente ItemTextArea

---
+++ web/src/__tests__/components/ItemTextArea.spec.tsx
@@ -0,0 +1,33 @@
+/**
+ * Test: Item Text Area
+ */
+
+import React from 'react';
+import { fireEvent, render, waitFor } from '@testing-library/react';
+
+import ItemTextArea from '../../components/InputAndButtons/ItemTextArea';
+
+jest.mock('@unform/core', () => {
+    return {
+        useField: () => ({
+            fieldName: '',
+            defaultValue: '',
+            registerField: jest.fn(),
+            error: '',
+        }),
+    };
+});
+
+describe('Component: Item Text Area', () => {
+    it('should be able to render component', () => {
+        // Render component
+        const { getByPlaceholderText } = render(<ItemTextArea
+          label="Label Example"
+          name="Name Example"
+          placeholder="Placeholder Example"
+        />)
+
+        // Check if component are renderized
+        expect(getByPlaceholderText('Placeholder Example')).toBeTruthy();
+    });
+});
