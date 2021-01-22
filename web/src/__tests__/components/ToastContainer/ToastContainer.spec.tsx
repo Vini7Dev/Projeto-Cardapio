31f87581d1cf771a890dfa71dfbd836a2643a4da
Vinícius Gabriel <viniciusgabriel.dev@gmail.com>
2021-01-22 17:05:14 -0200

    Criando o teste do componente ToastContainer

---
+++ web/src/__tests__/components/ToastContainer/ToastContainer.spec.tsx
@@ -0,0 +1,25 @@
+/**
+ * Test: Menu Footer
+ */
+
+import React from 'react';
+import { render, waitFor } from '@testing-library/react';
+
+import ToastContainer from '../../../components/ToastContainer';
+
+describe('Component: Toast Container', () => {
+    it('should be able to render component', async () => {
+        // Render component
+        const { getByText } = render(<ToastContainer
+          toasts={[ { id: '0', title: 'Title Example' } ]}
+        />);
+
+          // Select one toast element from component
+          const toast = getByText('Title Example');
+
+          // Expect element exists
+          await waitFor(() => {
+              expect(toast).toBeTruthy();
+          });
+    });
+});
