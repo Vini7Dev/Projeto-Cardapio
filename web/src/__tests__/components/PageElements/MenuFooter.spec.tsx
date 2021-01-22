a9676b1ae0d4b7f71f6ddb81e8e323cf026d2be1
Vinícius Gabriel <viniciusgabriel.dev@gmail.com>
2021-01-22 16:41:48 -0200

    Criando os testes do componente MenuFooter

---
+++ web/src/__tests__/components/PageElements/MenuFooter.spec.tsx
@@ -0,0 +1,27 @@
+/**
+ * Test: Menu Footer
+ */
+
+import { render, waitFor } from '@testing-library/react';
+import React from 'react';
+
+import MenuFooter from '../../../components/PageElements/MenuFooter';
+
+describe('Component: Menu Footer', () => {
+    it('should be able to render component', async () => {
+        // Render component
+        const { getByText } = render(<MenuFooter
+          trade="Trade Example"
+          cnpj="12345678910234"
+          telephone='99912345678'
+        />);
+
+        // Select one element from component
+        const element = getByText('Restaurante: Trade Example');
+
+        // Expect element exists
+        await waitFor(() => {
+            expect(element).toBeTruthy();
+        });
+    });
+});
